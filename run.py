8380269dca282d4307b9535615ddbf9a8e691181
Tor Yohanson <toryohanson@gmail.com>
2016-07-29 12:47:39 +0300

    Base application

--- run.py
+++ run.py
@@ -1,2 +1,9 @@
+import os
 from app import app
-app.run(debug=True)
+
+def main():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+
+if __name__ == '__main__':
+    main()
