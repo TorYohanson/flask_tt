6462a9bc128f2c1876c88d455521c9120b6bf926
Tor Yohanson <toryohanson@gmail.com>
2016-07-29 14:17:17 +0300

    Fifth commit: little bug fixed

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,7 @@ Takes information about population from http://media.mongodb.org/zips.json,
 stores it in MongoDB and gives a information about top-20 most populated cities on request to home directory.
 """
 import app.tools.mongo_tools as at
-
-from app import views
 from flask_wtf.csrf import CsrfProtect
-
 from flask import Flask
 
 # Creates DB connection and loads information from http://media.mongodb.org/zips.json into MongoDB collection.
@@ -15,6 +12,7 @@ cat = at.Catalog()
 cat.load_collection()
 
 app = Flask(__name__)
+from app import views
 
 # Enables CSRF protection for POST requests.
 app.config.update(
